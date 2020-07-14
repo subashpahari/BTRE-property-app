e4583c6d418632b62f5886b76bb9c9ddf85b11a0
Subash Pahari <subashpahari.12345@gmail.com>
2020-07-14 09:39:56 +0545

    last commit added import contact model

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 
+from contacts.models import Contact
 
 from django.contrib.auth.models import User
 
@@ -68,5 +69,9 @@ def logout(request):
         return redirect('index')
 
 def dashboard(request):
-    
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context = {
+    'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
